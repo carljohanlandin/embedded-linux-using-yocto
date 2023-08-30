bc6a12c03302e761bdc1c92a361814435c38ac32
Carl-Johan Landin <carl-johan.landin@endian.se>
2023-08-30 12:49:20 +0200

    Add recipe with dummy shared lib

---
+++ meta-mylayer/recipes-example/mysharedlib/files/mylib.h
@@ -0,0 +1,10 @@
+#ifndef __LIB_H
+#define __LIB_H
+
+int add(int num1, int num2);
+int sub(int num1, int num2);
+int mul(int num1, int num2);
+int div(int num1, int num2);
+void print(char *str, int times);
+
+#endif
---
+++ meta-mylayer/recipes-example/mysharedlib/files/tmp/userprog.c
@@ -0,0 +1,7 @@
+#include "mylib.h"
+#include <stdio.h>
+
+int main() {
+  print("Hello world!\n", 5);
+  return 0;
+}
